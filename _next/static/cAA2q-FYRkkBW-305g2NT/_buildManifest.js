063a5dbef516c58eea25957f230f97e82c9ff9e6
Redskull-127 <76103614+Redskull-127@users.noreply.github.com>
2022-06-28 13:59:26 +0000

    Deploying to gh-pages from @ Redskull-127/QR-code-maker-@e2f610123c200a46272b291f8962cefe9a94da80 🚀

---
+++ _next/static/cAA2q-FYRkkBW-305g2NT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/666ab1814f028286.css","static/chunks/pages/index-ec56acfc8d40c72f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
